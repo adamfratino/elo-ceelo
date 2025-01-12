d239399c0d6b7c4e5493b80f70f81d5e5793c622
Adam Fratino <fratino.a@gmail.com>
2025-01-12 16:01:44 -0500

    number-flow

--- app/components/EloRating.tsx
+++ app/components/EloRating.tsx
@@ -1,21 +1,33 @@
 "use client";
 
+import NumberFlow from "@number-flow/react";
 import { Shuffle } from "lucide-react";
 
 import { useEloStore } from "@/app/stores/elo";
+import { generateOpponentRating } from "@/app/utils/elo";
 
 export const EloRating = () => {
-  const { heroRating, villainRating } = useEloStore();
+  const { heroRating, villainRating, setVillainRating } = useEloStore();
+
+  function handleShuffle() {
+    const newVillainRating = generateOpponentRating(heroRating);
+    setVillainRating(newVillainRating);
+  }
 
   return (
     <div className="flex justify-between items-center gap-4 mt-2">
-      <h2 className="font-bold uppercase text-sm">You: {heroRating}</h2>
-      <Shuffle size={16} stroke="white" />
       <h2 className="font-bold uppercase text-sm">
-        Them:{" "}
-        <span className="inline-block min-w-[4ch]">
-          {villainRating ?? "0000"}
-        </span>
+        You: <NumberFlow value={heroRating} />
+      </h2>
+      <button
+        type="button"
+        onClick={handleShuffle}
+        className="appearance-none border-2 border-foreground rounded-md p-2 hover:scale-105 active:scale-95 transition-all"
+      >
+        <Shuffle size={12} stroke="white" className="transition-all" />
+      </button>
+      <h2 className="font-bold uppercase text-sm">
+        Them: <NumberFlow value={villainRating} />
       </h2>
     </div>
   );
--- app/stores/elo.ts
+++ app/stores/elo.ts
@@ -2,10 +2,12 @@ import { create } from "zustand";
 
 interface EloState {
   heroRating: number;
-  villainRating?: number;
+  villainRating: number;
+  setVillainRating: (rating: number) => void;
 }
 
 export const useEloStore = create<EloState>((set) => ({
   heroRating: 1500,
-  villainRating: undefined,
+  villainRating: 1500,
+  setVillainRating: (rating) => set(() => ({ villainRating: rating })),
 }));
