3cb3071581013ed9219c68aefb7d4d003ef7149d
Adam Fratino <fratino.a@gmail.com>
2025-01-16 00:46:22 -0500

    fix elo, add multiplier, some color

--- lib/components/VillainDetails.tsx
+++ lib/components/VillainDetails.tsx
@@ -8,19 +8,35 @@ import { cn } from "@/lib/utils/cn";
 import { Die } from "./Die";
 
 export const VillainDetails = () => {
-  const { villainRoll = [0, 0, 0], villainScore } = useMatchStore();
+  const { villainRoll, villainScore, result } = useMatchStore();
 
   return (
     <div className="flex flex-col items-end">
-      <span className="-mb-1">their score:</span>
-      <NumberFlow value={villainScore ?? 0} className="text-4xl -my-1" />
+      <span>their score:</span>
+      <NumberFlow
+        value={villainScore ?? 0}
+        className={cn("text-4xl h-14", {
+          "text-negative": result === "win",
+          "text-positive": result === "lose",
+          "text-neutral": result === "draw",
+        })}
+      />
       <div
-        className={cn("flex opacity-100 transition-all duration-150", {
+        className={cn("flex opacity-100 transition-all", {
           "opacity-0": !villainScore,
         })}
       >
-        {villainRoll.map((num, i) => (
-          <Die num={num} size={28} key={`${num}_${i}`} />
+        {villainRoll?.map((num, i) => (
+          <Die
+            num={num}
+            size={28}
+            key={`${num}_${i}`}
+            className={cn({
+              "stroke-negative": result === "win",
+              "stroke-positive": result === "lose",
+              "stroke-neutral": result === "draw",
+            })}
+          />
         ))}
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./lib/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    colors: {
+      background: "var(--background)",
+      foreground: "var(--foreground)",
+      positive: "var(--positive)",
+      negative: "var(--negative)",
+      neutral: "var(--neutral)",
     },
   },
   plugins: [],
